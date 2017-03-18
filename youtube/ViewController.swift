7063bed0e408541007668a9ece26d174a5d4ce5a
南希 <447108518@qq.com>
2017-03-19 00:21:32 +0800

    finish base cell

--- youtube/ViewController.swift
+++ youtube/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UICollectionViewController,UICollectionViewDelegateFlowLay
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "视频首页"
         self.collectionView?.backgroundColor = UIColor.white
         self.collectionView?.register(YouTuBeCell.self, forCellWithReuseIdentifier: "cellID")
     }
@@ -57,25 +58,82 @@ extension ViewController{
 class YouTuBeCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         setUpView()
+        
     }
+    
     
     let thumbnailImageView : UIImageView = {
         let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-//        imageView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
         return imageView
     }()
     
+    //添加cell分割线
+    let separateLine : UIView = {
+        let lineView = UIView()
+        lineView.backgroundColor = .black
+        return lineView
+    }()
     
+    let userProfilerImage : UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .green
+        return imageView
+    }()
+    
+    let titleLabel :UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.purple
+        return label
+    }()
+    
+    let subTitle : UITextView = {
+        let subTitle = UITextView()
+        subTitle.translatesAutoresizingMaskIntoConstraints = false
+        subTitle.backgroundColor = .red
+        return subTitle
+    }()
     func setUpView(){
         addSubview(thumbnailImageView)
-//        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":thumbnailImageView]))
-//        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[v0]-10-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":thumbnailImageView]))
+        addSubview(separateLine)
+        addSubview(userProfilerImage)
+        addSubview(titleLabel)
+        addSubview(subTitle)
+        
+        
         addConstraintsWithFromart(format: "H:|-10-[v0]-10-|", views: thumbnailImageView)
-        addConstraintsWithFromart(format: "V:|-10-[v0]-10-|", views: thumbnailImageView)
+        addConstraintsWithFromart(format: "V:|-10-[v0]-10-[v1(44)]-10-[v2(1)]|", views: thumbnailImageView,userProfilerImage,separateLine)
+        addConstraintsWithFromart(format: "H:|[v0]|", views: separateLine)
+        addConstraintsWithFromart(format: "H:|-10-[v0(44)]|", views: userProfilerImage)
+        
+        
+        //MARK:-  A 视图    =      B视图  *  multiplier   +   constant；
+
+        //top
+        addConstraints([NSLayoutConstraint.init(item: titleLabel, attribute: .top, relatedBy: NSLayoutRelation.equal, toItem: thumbnailImageView, attribute: .bottom, multiplier: 1, constant: 10)])
+        
+        //left
+        addConstraints([NSLayoutConstraint.init(item: titleLabel, attribute: .left, relatedBy: .equal, toItem: userProfilerImage, attribute: .right, multiplier: 1, constant: 8)])
+        //height
+        addConstraints([NSLayoutConstraint.init(item: titleLabel, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20)])
+        //right
+        addConstraints([NSLayoutConstraint.init(item: titleLabel, attribute: .right, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 0)])
+        
+        
+        //MARK:- subTitle
+        //top
+        addConstraints([NSLayoutConstraint.init(item: subTitle, attribute: .top, relatedBy: .equal, toItem: titleLabel, attribute: .bottom, multiplier: 1, constant: 8)])
+        //left
+        addConstraints([NSLayoutConstraint.init(item: subTitle, attribute: .left, relatedBy: .equal, toItem: titleLabel, attribute: .left, multiplier: 1, constant: 0)])
+        //right
+        addConstraints([NSLayoutConstraint.init(item: subTitle, attribute: .right, relatedBy: .equal, toItem: titleLabel, attribute: .right, multiplier: 1, constant: 0)])
+        //height
+        addConstraints([NSLayoutConstraint.init(item: subTitle, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20)])
+
+        
         thumbnailImageView.backgroundColor = .orange
+        
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -92,6 +150,7 @@ extension UIView {
         
         for (index,view) in views.enumerated() {
             let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
             viewDictionary[key] = view
         }
         
